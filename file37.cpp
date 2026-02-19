0d632fed63377331acbeba41612f914dd46a3896
Abhyuday Gautam <abgautam1903@gmail.com>
2026-02-19 20:31:40 +0530

    Recurse numbers opposite

---
+++ file37.cpp
@@ -0,0 +1,20 @@
+// recursive function to print from n to 1
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n)
+{
+    if(n==0)
+    {
+        return;
+    }
+    cout<<n<<" ";
+    print(n-1);
+}
+int main()
+{
+    int n;
+    cout<<"Enter a number: "<<endl;
+    cin>>n;
+    print(n);
+    return 0;
+}
