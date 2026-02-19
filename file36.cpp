3d51a7cdbc270fe82490b890feb2a212266375c9
Abhyuday Gautam <abgautam1903@gmail.com>
2026-02-19 20:26:03 +0530

    Recurse numbers linearly

---
+++ file36.cpp
@@ -0,0 +1,20 @@
+// recursion to print numbers from 1 to n linearly
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n)
+{
+    if(n==0)
+    {
+        return;
+    }
+    print(n-1);
+    cout<<n<<" ";
+}
+int main()
+{
+    int n;
+    cout<<"Enter a number: "<<endl;
+    cin>>n;
+    print(n);
+    return 0;
+}
