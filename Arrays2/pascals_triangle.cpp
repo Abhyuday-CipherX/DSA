c9736134037708ea6e3b88cd0d0df41839b63b9c
Abhyuday Gautam <abgautam1903@gmail.com>
2026-03-05 00:12:06 +0530

    Pascal's Triangle

---
+++ Arrays2/pascals_triangle.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class solution
+{
+    public:
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> ans(numRows);
+        for (int i = 0; i < numRows; i++)
+        {
+            ans[i].resize(i + 1);
+            ans[i][0] = ans[i][i] = 1;
+            for (int j = 1; j < i; j++)
+            {
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
+        }
+        return ans;
+    }
+};
+int main()
+{
+    int numRows;
+    cin >> numRows;
+    solution ob;
+    vector<vector<int>> ans = ob.generate(numRows);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
